c830f33304888ee0e765b7353074176a0b197dee
phitabs24 <107364663+phitabs24@users.noreply.github.com>
2022-09-13 05:51:10 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
-
 /**
- * main - prints the numbers from 00 to 99
- * Return: Always 0 (success)
+ * main - program that prints all possible different combinations of 2 digits
+ * Return: 0
  */
 int main(void)
 {
-	int n, m;
+	int c = 0;
+	int f_d;
+	int l_d;
 
-	for (n = 48; n <= 57; n++)
+	while (c <= 99)
 	{
-		for (m = 48; m <= 57; m++)
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+
+		if (f_d < l_d)
 		{
-			putchar(n);
-			putchar(m);
-			if (n != 57 || m != 57)
+			putchar(f_d);
+			putchar(l_d);
+
+			if (c != 89)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
+		c++;
 	}
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
