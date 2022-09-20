354831ee0851e87ea9f55c0f7f947c729f057a14
phitabs24 <107364663+phitabs24@users.noreply.github.com>
2022-09-20 16:02:07 +0100

    Task 0 completed

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: a character whose case is to be checked
+ * Return: 1 if c is uppercase
+ * or 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the readme for the Project; More Fuctions, More nested loops
