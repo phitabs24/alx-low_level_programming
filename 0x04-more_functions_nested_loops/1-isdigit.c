32652254c3178b9be4864915ef20ec358e695310
phitabs24 <107364663+phitabs24@users.noreply.github.com>
2022-09-20 23:02:07 +0100

    debugged task 1 again

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,21 +2,10 @@
 
 /**
  * _isdigit - checks for a digit (0 through 9)
- *
- * @c: a character whose case is to be checked
- * Return: 1 if c is uppercase
- * or 0 otherwise
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-	{
-		_putchar(1);
-	}
-	else
-	{
-		_putchar(0);
-	}
-	/*return (c >= 0 && c <= 9);*/
-	return (0);
+	return (c >= '0' && c <= '9');
 }
